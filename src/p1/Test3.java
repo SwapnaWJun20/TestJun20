a5ea40a6b7ede1d8fbffc5a88f4b123e25d40f79
Swapna Wairagade <wairagadeswapna@gmail.com>
2020-08-07 00:14:47 +0530

    Test

---
+++ src/p1/Test3.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class Test3 {
+	private void m2() {
+		System.out.println("IN Test3 - M2");
+		System.out.println("Hello");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(" " );
+	}
+}
